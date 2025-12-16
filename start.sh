ef650bc336ccd153de0159cc998369d157e24e2f
Laurent CHEVOULINE <laurent@tahiticonnect.com>
2025-12-15 20:35:40 -1000

    Use Lucterios standalone server

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ mkdir -p /var/lucterios2/data
 if [ -f "setup.py" ]; then
     pip install --no-cache-dir -e .
 fi
-echo "✅ Starting Gunicorn..."
-exec gunicorn --bind 0.0.0.0:${PORT} --workers 2 --worker-class sync --timeout 120 --access-logfile - --error-logfile - wsgi:application
+cd /app
+exec python -m lucterios.standalone --port=${PORT}
