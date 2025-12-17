b117d18d679ec59c5986c0d9691eb3901694d9bf
Laurent CHEVOULINE <laurent@tahiticonnect.com>
2025-12-16 18:10:15 -1000

    Add wsgi.py for gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,18 @@
 """
-Diacamma WSGI application for Railway
+Diacamma WSGI application for Railway deployment
 """
 import os
 import sys
+import django
+
 sys.path.insert(0, '/app')
 
-# Setup Django
 from django.conf import settings
 from lucterios.install import get_settings_django
 
 if not settings.configured:
     settings.configure(**get_settings_django())
 
-import django
 django.setup()
 
 from django.core.wsgi import get_wsgi_application
